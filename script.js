2858d1ac0fe54a14535cf68d5b3629a89a1524e3
SamRosati <67395223+SamRosati@users.noreply.github.com>
2025-10-30 15:00:21 -0700

    add files via upload
    html file may need to be altered since images folder is now not included

---
+++ script.js
@@ -0,0 +1,7 @@
+// Toggle navigation only on mobile view
+function myFunction() {
+    if (window.innerWidth <= 768) {
+      document.getElementById("myLinks").classList.toggle("show");
+    }
+  }
+  
